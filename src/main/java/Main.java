073275c0f4a39a35e2aca5d85e47fa6ecff06dba
arnoldout <arnoldout@gmail.com>
2016-09-30 18:55:56 +0100

    Connection made to mongo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,25 +2,29 @@ package main.java;
 
 import static spark.Spark.get;
 import static spark.SparkBase.port;
+
+import java.util.Arrays;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoDatabase;
 public class Main {
 	
     @SuppressWarnings("resource")
 	public static void main(String[] args) {
     	//for running locally, remove this port line
-    	port(Integer.valueOf(System.getenv("PORT")));
+    	//port(Integer.valueOf(System.getenv("PORT")));
     	
     	//basic help response to a blank call to the webpage
     	get("/", (request, response) -> 
     	{
-    		MongoClientURI mongoClientURI = new MongoClientURI("mongodb://heroku_s4r2lcpf:mongopassword1@ds035026.mlab.com:35026/heroku_s4r2lcpf");
-    		MongoClient mongoClient = new MongoClient(mongoClientURI);
-    		MongoDatabase db = mongoClient.getDatabase("test");
+    		MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://arnoldout111:mongopassword1@ds035026.mlab.com:35026/heroku_s4r2lcpf"));
     		
-    		System.out.println("Connect to database successfully");
-            return "asds";
-		});
+    		return "asds";
+		});	
     }
 }

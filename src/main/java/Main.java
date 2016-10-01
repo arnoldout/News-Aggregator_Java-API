aceb64993f30475a849ba9139cad79f764643494
arnoldout <arnoldout@gmail.com>
2016-10-01 14:56:07 +0100

    Pipeline Setup
    API now talks to mongo and returns data depending on if the user account
    is new or taken already

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,24 @@
 package main.java;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.bson.Document;
+import org.json.JSONException;
+
+import com.google.gson.Gson;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.util.JSON;
 
 import services.MongoConnection;
 import services.ProfileService;
+import types.Profile;
 public class Main {
 	
 	public static void main(String[] args) {
@@ -13,15 +28,42 @@ public class Main {
     	//basic help response to a blank call to the webpage
     	get("/", (request, response) -> 
     	{
-    		MongoConnection mc = new MongoConnection("mongodb://arnoldout111:mongopassword1@ds035026.mlab.com:35026/heroku_s4r2lcpf", "heroku_s4r2lcpf");
-    		ProfileService ps = new ProfileService(mc.getDb());
     		return "asds";
 		});
     	post("/addProfile", (request, response) -> 
     	{
-    		String s = request.body();
-    		s = s;
-    		return "Hello World: " + request.body();
+    		Gson g = new Gson();
+    		
+    		MongoConnection mc = new MongoConnection("mongodb://arnoldout111:mongopassword1@ds035026.mlab.com:35026/heroku_s4r2lcpf", "heroku_s4r2lcpf");
+    		ProfileService ps = new ProfileService(mc.getDb());
+    		MongoCollection<Document> col = ps.getCollection("profile");
+    		//make sure json is a valid Pofile JSON object
+    		Document dbo = null;
+    		try{
+    			Profile p = g.fromJson(request.body(), Profile.class);
+    			dbo = p.makeDocument();
+    		}
+    		catch(JSONException e)
+    		{
+    			response.status(406);
+    			return response;
+    		}
+    		FindIterable<Document>docs = col.find();
+    		for(Document p : docs)
+    		{
+    			Collection<Object> o = p.values();
+    			String s = (String) p.get("username");
+    			if(s.equals(dbo.get("username")))
+    			{
+    				//name taken
+    				return false;
+    			}
+    		}
+    		col.insertOne(dbo);
+    		return dbo.get("id").toString();
     	});
     }
 }
+
+
+
---
+++ src/types/Profile.java
@@ -0,0 +1,42 @@
+package types;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+public class Profile {
+	private ObjectId id;
+	private String username;
+	private String password;
+
+	public Profile(String username, String password) {
+		super();
+		id = new ObjectId();
+		this.username = username;
+		this.password = password;
+	}
+	public Profile() {
+		super();
+		id = new ObjectId();
+	}
+	public Document makeDocument()
+	{
+		Document d = new Document();
+		//if adding more fields, this needs to also be edited
+		d.append("username", username);
+		d.append("password", password);
+		d.append("id", id);
+		return d;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}

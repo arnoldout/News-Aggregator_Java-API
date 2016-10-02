c2c90a7b709355935382153aada60fba1e09c8a1
arnoldout <arnoldout@gmail.com>
2016-10-02 10:44:43 +0100

    Route Added
    Route added, working on requesting document from mongo by the objectid
    now

--- src/com/ie/sw/main/Main.java
+++ src/com/ie/sw/main/Main.java
@@ -1,5 +1,7 @@
 package com.ie.sw.main;
 
+import static com.mongodb.client.model.Filters.*;
+
 import static spark.Spark.get;
 import static spark.Spark.post;
 import static spark.SparkBase.port;
@@ -10,25 +12,35 @@ import org.json.JSONException;
 import com.google.gson.Gson;
 import com.ie.sw.services.*;
 import com.ie.sw.types.*;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 public class Main {
 	
 	public static void main(String[] args) {
     	//for running locally, remove this port line
-    	port(Integer.valueOf(System.getenv("PORT")));
+    	//port(Integer.valueOf(System.getenv("PORT")));
+    	
+    	MongoConnection mc = new MongoConnection("mongodb://arnoldout111:mongopassword1@ds035026.mlab.com:35026/heroku_s4r2lcpf", "heroku_s4r2lcpf");
+		ProfileService ps = new ProfileService(mc.getDb());
     	
     	//basic help response to a blank call to the webpage
     	get("/", (request, response) -> 
     	{
-    		return "asds";
+    		return "null";
+		});
+    	get("/getProfile/:profileId", (request, response) -> 
+    	{
+    		String id = request.params(":profileId");
+    		MongoCollection<Document> col = ps.getCollection("profile");
+    		Document d = col.find(eq("_id", id)).first();
+    		return d.toString();
 		});
     	post("/addProfile", (request, response) -> 
     	{
     		Gson g = new Gson();
     		
-    		MongoConnection mc = new MongoConnection("mongodb://arnoldout111:mongopassword1@ds035026.mlab.com:35026/heroku_s4r2lcpf", "heroku_s4r2lcpf");
-    		ProfileService ps = new ProfileService(mc.getDb());
     		MongoCollection<Document> col = ps.getCollection("profile");
     		//make sure JSON is a valid Profile JSON object
     		Document dbo = null;

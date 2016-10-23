8a516924f67ad57d8e6f0db637d010e292d81b7f
arnoldout <arnoldout@gmail.com>
2016-10-23 22:45:05 +0100

    Fixed many bugs

--- src/main/java/factory/TaggingFactory.java
+++ src/main/java/factory/TaggingFactory.java
@@ -2,6 +2,9 @@ package main.java.factory;
 
 import static com.mongodb.client.model.Filters.eq;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -17,6 +20,7 @@ import com.mongodb.client.MongoCollection;
 import main.java.services.ArticleService;
 import main.java.services.GsonWrapper;
 import main.java.services.MongoConnection;
+import main.java.services.ProfileService;
 import main.java.services.TaggingService;
 import main.java.types.Profile;
 import main.java.types.Story;
@@ -26,12 +30,14 @@ public class TaggingFactory {
 
 	private TaggingService ts;
 	private ArticleService as;
+	private ProfileService ps;
 	private MongoConnection mc;
 	public TaggingFactory(MongoConnection mc) {
 		super();
 		this.mc = mc;
 		this.ts = new TaggingService(mc);
 		this.as = new ArticleService(mc);
+		this.ps = new ProfileService(mc);
 	}
 
 	public void generateTags(Story story)
@@ -63,11 +69,12 @@ public class TaggingFactory {
 		Gson g = gw.getGson();
 		MongoCollection<Document> keyPairs = mc.getDb().getCollection("tagPairs");
 		Set<String> uniqueUris = new HashSet<String>();
+		List<TagViewPair> tvps = new ArrayList<TagViewPair>();
 		for(String id : p.getLikes())
 		{
 			Document tagPair = (Document) keyPairs.find(eq("_id", new ObjectId(id))).first();
 			TagViewPair tvp = g.fromJson(tagPair.toJson(), TagViewPair.class);
-		
+			tvps.add(tvp);
 			Document d = (Document) ts.getCol().find(eq("name", tvp.getTag())).first();
 			@SuppressWarnings("unchecked")
 			List<ObjectId> tagArticles = (List<ObjectId>) d.get("articles");
@@ -80,6 +87,18 @@ public class TaggingFactory {
 				}
 			}
 		}
+		Collections.sort(tvps, new Comparator<TagViewPair>() {
+	        @Override
+	        public int compare(TagViewPair o1, TagViewPair o2) {
+	            return Integer.compare(o2.getViewCount(), o1.getViewCount());
+	        }
+	    });
+		p.clearLikes();
+		for(TagViewPair tvp : tvps)
+		{
+			p.addLike(tvp.get_id());
+		}
+		ps.updateProfile(p);
 		return ja;
 	}
 }

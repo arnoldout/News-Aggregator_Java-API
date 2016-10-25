d99b2528106f99adea79e56a39c1bc92c15eff27
arnoldout <arnoldout@gmail.com>
2016-10-25 18:47:43 +0100

    Added Sky News and GetWestLondon as sources

--- src/main/java/types/SkyNewsDoc.java
+++ src/main/java/types/SkyNewsDoc.java
@@ -5,25 +5,24 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-public class NYTDoc extends XMLDoc {
+public class SkyNewsDoc extends XMLDoc {
 
-	public final String url = "http://rss.nytimes.com/services/xml/rss/nyt/MostViewed.xml";
+	public final String url = "http://feeds.skynews.com/feeds/rss/world.xml";
 
 	@Override
 	public void parseXml() {
 		Document xmlReader = getXML(this.url);
 
-		NodeList nList = xmlReader.getElementsByTagName("item");
+		NodeList nList = xmlReader.getElementsByTagName("entry");
 
 		for (int temp = 0; temp < nList.getLength(); temp++) {
 			Node nNode = nList.item(temp);
 			Element eElement = (Element) nNode;
-			Story item = new Story(eElement.getElementsByTagName("guid").item(0).getTextContent()+"?smid=fb-nytimes&smtyp=cur");
+			Story item = new Story(eElement.getElementsByTagName("guid").item(0).getTextContent());
 			item.setTitle(eElement.getElementsByTagName("title").item(0).getTextContent());
 			item.setDescription(eElement.getElementsByTagName("description").item(0).getTextContent());
-			item.setImgUri((eElement.getElementsByTagName("media:content").item(0)).getAttributes().getNamedItem("url").getNodeValue());
+			item.setImgUri((eElement.getElementsByTagName("media:thumbnail").item(0)).getAttributes().getNamedItem("url").getNodeValue());			//no categories, word cloud needed
 			super.add(item);
 		}
-
 	}
 }
